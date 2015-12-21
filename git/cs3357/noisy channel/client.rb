e8f0b719d57d756a65e1b3318bfaafc5f556dc8f
Charlie Zhao <wzhao67@uwo.ca>
2015-12-21 16:44:32 -0500

    removed duplicate

--- git/cs3357/noisy channel/client.rb
+++
@@ -1,6 +0,0 @@
-require 'socket'
-s = UDPSocket.new
-
-20.times do |i|
-  s.send("Client message #{i+1}", 0, 'localhost', 5000)
-end
--- git/cs3357/noisy channel/server.rb
+++
@@ -1,9 +0,0 @@
-require 'socket'
-
-s = UDPSocket.new
-s.bind(nil, 5000)
-
-20.times do |i|
-  text, sender = s.recvfrom(100)
-  puts "Message #{i+1}: #{text}"
-end
